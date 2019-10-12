4532a41699315e9ef3eaa8582d71cc22fbb774fd
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-10-12 13:16:27 +0800

    Implement Processor completely

--- coherence/src/coherence/devices/Processor.scala
+++ coherence/src/coherence/devices/Processor.scala
@@ -6,6 +6,14 @@ object Processor {
   def apply[Message, State](cache: Cache[Message, State],
                             source: Source): Processor[Message, State] =
     new Processor(cache, source.getLines().map(ProcessorOp(_)))
+
+  sealed trait Status
+  object Status {
+    case class Ready() extends Status
+    case class Operation(untilCycle: Long) extends Status
+    case class Cache() extends Status
+    case class Finished() extends Status
+  }
 }
 
 class Processor[Message, State](private[this] val cache: Cache[Message, State],
@@ -14,9 +22,51 @@ class Processor[Message, State](private[this] val cache: Cache[Message, State],
     with Device {
   private[this] var currentCycle: Long = 0
 
+  private[this] var currentOp: Option[ProcessorOp] = None
+  private[this] var status: Processor.Status = Processor.Status.Ready()
+
   override def cycle(): Unit = {
     currentCycle += 1
+    cache.cycle()
+    status match {
+      case Processor.Status.Finished() | Processor.Status.Cache() => ()
+      case Processor.Status.Operation(untilCycle) =>
+        if (currentCycle > untilCycle) {
+          status = Processor.Status.Ready()
+          currentOp = None
+        }
+        performInstruction()
+      case Processor.Status.Ready() =>
+        performInstruction()
+    }
   }
 
-  override def requestCompleted(op: CacheOp): Unit = ???
+  private[this] def performInstruction(): Unit = {
+    if (currentOp.isEmpty) loadInstruction()
+    currentOp match {
+      case None => status = Processor.Status.Finished()
+      case Some(ProcessorOp.Other(numCycles)) =>
+        status =
+          Processor.Status.Operation(untilCycle = currentCycle + numCycles)
+      case Some(ProcessorOp.Load(address)) =>
+        status = Processor.Status.Cache()
+        cache.request(this, CacheOp.Load(address))
+      case Some(ProcessorOp.Store(address)) =>
+        status = Processor.Status.Cache()
+        cache.request(this, CacheOp.Store(address))
+    }
+  }
+
+  private[this] def loadInstruction(): Unit = {
+    if (ops.hasNext) currentOp = Some(ops.next())
+    else currentOp = None
+  }
+
+  override def requestCompleted(op: CacheOp): Unit =
+    currentOp match {
+      case Some(ProcessorOp.Load(_)) | Some(ProcessorOp.Store(_)) =>
+        status = Processor.Status.Ready()
+      case _ =>
+        throw new RuntimeException("Got response from cache without request")
+    }
 }
