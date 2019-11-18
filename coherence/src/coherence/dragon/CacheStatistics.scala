9ca0b3082ae46ca6855d33f27b70f00f0c2b7bd5
Kyle Chu <ktnc.985@gmail.com>
2019-11-18 15:34:46 +0800

    add simulation for dragon protocol

---
+++ coherence/src/coherence/dragon/CacheStatistics.scala
@@ -0,0 +1,19 @@
+package coherence.dragon
+
+import coherence.devices.{CacheStatistics => AbstractCacheStatistics}
+
+class CacheStatistics[State] extends AbstractCacheStatistics[State] {
+  private[this] var _numPrivateAccess: Long = 0
+  private[this] var _numSharedAccess: Long = 0
+
+  override def numPrivateAccess: Long = _numPrivateAccess
+  override def numSharedAccess: Long = _numSharedAccess
+
+  override def logState(state: State): Unit =
+    state match {
+      case _: State.M.type | _: State.E.type => _numPrivateAccess += 1
+      case _: State.SC.type | _: State.SM.type => _numSharedAccess += 1
+      case _: State.I.type =>
+        throw new RuntimeException("Unexpected state I logged")
+    }
+}
---
+++ coherence/src/coherence/dragon/Memory.scala
@@ -0,0 +1,63 @@
+package coherence.dragon
+
+import coherence.Debug._
+import coherence.Address
+import coherence.bus.{Bus, BusDelegate, MessageMetadata, ReplyMetadata}
+import coherence.devices.{Memory => AbstractMemory}
+
+class Memory(bus: Bus[Message, Reply], blockSize: Int)
+    extends AbstractMemory[State, Message, Reply](bus, blockSize) {
+  override def busAccessGranted(): MessageMetadata[Message] =
+    throw new RuntimeException("Memory: unexpected busAccessGranted()")
+
+  override def onBusCompleteMessage(sender: BusDelegate[Message, Reply],
+                                    address: Address,
+                                    message: Message): Unit = {
+    require(maybeReply.isEmpty)
+    println_debug(s"Memory: before, maybeReply = $maybeReply")
+    message match {
+      case Message.BusUpt() => ()
+      case Message.Flush() =>
+        maybeReply = Some(
+          (
+            ReplyMetadata(Reply.WriteBackOk(), 1),
+            currentCycle + AbstractMemory.Latency
+          )
+        )
+      case Message.BusRd() =>
+        maybeReply = Some(
+          (
+            ReplyMetadata(Reply.MemoryRead(), blockSize),
+            currentCycle + AbstractMemory.Latency
+          )
+        )
+    }
+    println_debug(s"Memory: after, maybeReply = $maybeReply")
+  }
+
+  override def onBusCompleteResponse(
+    sender: BusDelegate[Message, Reply],
+    address: Address,
+    reply: Reply,
+    originalSender: BusDelegate[Message, Reply],
+    originalMessage: Message
+  ): Unit = reply match {
+    case Reply.Flush() =>
+      maybeReply match {
+        case Some((ReplyMetadata(Reply.MemoryRead(), _), _)) =>
+          maybeReply = Some(
+            (
+              ReplyMetadata(Reply.WriteBackOk(), 1),
+              currentCycle + AbstractMemory.Latency
+            )
+          )
+        case None | Some(_) =>
+          throw new RuntimeException(
+            s"Memory: received Flush() while maybeReply is $maybeReply"
+          )
+      }
+    case Reply.MemoryRead() | Reply.WriteBackOk() =>
+      throw new RuntimeException(s"Memory: unexpected reply $reply")
+  }
+
+}
